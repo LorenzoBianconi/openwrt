45587f5ab712dfbffa216b15699686cd029586c0
Christian Marangi <ansuelsmth@gmail.com>
2024-08-18 10:24:26 +0200

    airoha: add support for EIP93
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

---
+++ target/linux/airoha/files/drivers/crypto/mtk-eip93/eip93-common.h
@@ -0,0 +1,23 @@
+/* SPDX-License-Identifier: GPL-2.0
+ *
+ * Copyright (C) 2019 - 2021
+ *
+ * Richard van Schagen <vschagen@icloud.com>
+ */
+
+#ifndef _EIP93_COMMON_H_
+#define _EIP93_COMMON_H_
+
+#include "eip93-main.h"
+
+void *mtk_get_descriptor(struct mtk_device *mtk);
+
+void mtk_set_sa_record(struct sa_record *sa_record, const unsigned int keylen,
+		       const u32 flags);
+
+int mtk_parse_ctrl_stat_err(struct mtk_device *mtk, int err);
+
+int mtk_authenc_setkey(struct crypto_shash *cshash, struct sa_record *sa,
+		       const u8 *authkey, unsigned int authkeylen);
+
+#endif /* _EIP93_COMMON_H_ */
