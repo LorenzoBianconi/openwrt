7f5b82cba84dd47127bbc4d2601816fb8ee65551
Lorenzo Bianconi <lorenzo@kernel.org>
2024-10-07 00:17:29 +0200

    HACK: update ets-qdisc.sh for testing QoS offload (REMOVE)
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- target/linux/airoha/base-files/etc/ets-qdisc.sh
+++ target/linux/airoha/base-files/etc/ets-qdisc.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 ifconfig lan1 192.168.83.115 up
-tc qdisc replace dev eth0 root handle 1: ets bands 8 strict 6 quanta 2360 1514
-tc qdisc add dev eth0 clsact
+tc qdisc replace dev eth0 root handle 1: ets bands 8 strict 8
 
-tc filter add dev eth0 protocol ip egress flower ip_proto tcp dst_port 6001 action skbedit queue 0
-tc filter add dev eth0 protocol ip egress flower ip_proto tcp dst_port 6002 action skbedit queue 1
+tc filter add dev eth0 protocol ip parent 1: flower ip_proto tcp dst_port 6001 action skbedit queue 0
+tc filter add dev eth0 protocol ip parent 1: flower ip_proto tcp dst_port 6002 action skbedit queue 1
 
 tc qdisc show dev eth0
-tc filter show dev eth0 egress
+tc filter show dev eth0
