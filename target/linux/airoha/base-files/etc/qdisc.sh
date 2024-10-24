8769a4d1d0fe7b09da0bbba3577745ba803a5367
Lorenzo Bianconi <lorenzo@kernel.org>
2024-10-24 18:32:41 +0200

    HACK: update qos scripts (REMOVE)
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- target/linux/airoha/base-files/etc/qdisc.sh
+++ target/linux/airoha/base-files/etc/qdisc.sh
@@ -2,7 +2,7 @@
 
 IP=192.168.83.115
 DST=192.168.83.120
-RATE=200000
+RATE=100000
 NSTRICT=8
 #QUANTA="quanta 3528 1514 1514 1514 1514 1514"
 QUANTA=""
@@ -23,9 +23,10 @@ ip link set dev br0 up
 sleep 2
 ping -c 10 $DST
 sleep 2
-for i in $(seq 8); do
-echo $RATE > /sys/class/net/eth0/queues/tx-$i/tx_maxrate
-done
+#for i in $(seq 8); do
+#echo $RATE > /sys/class/net/eth0/queues/tx-$i/tx_maxrate
+#done
+echo $RATE > /sys/class/net/eth0/queues/tx-1/tx_maxrate
 
 tc filter del dev eth0 egress
 # Comment it out to be fully offloaded
