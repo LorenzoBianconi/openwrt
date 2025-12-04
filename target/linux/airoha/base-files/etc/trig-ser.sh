9fbf8eba0b9e32580a53f0042ebab8e2e435e818
Lorenzo Bianconi <lorenzo@kernel.org>
2025-12-04 08:38:00 +0100

    mt76: remove SER for the moment
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- target/linux/airoha/base-files/etc/trig-ser.sh
+++ target/linux/airoha/base-files/etc/trig-ser.sh
@@ -3,6 +3,6 @@
 DIR=/sys/kernel/debug/ieee80211/phy0/mt76/
 [ $# -ge 1 ] && BAND=$1 || BAND=1
 
-echo 0x00401484 > $DIR/regidx
+echo 0x004014ac > $DIR/regidx
 echo 0x15100002 > $DIR/regval
 echo "$BAND,1" > $DIR/sys_recovery
